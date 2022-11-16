c09ce60aef74c47e4bd60044d67187bce9a5bd3d
MajorFemi <harlaohorluwafemi@gmail.com>
2022-11-16 14:57:21 -0800

    bug fix

--- search_cwd.c
+++ search_cwd.c
@@ -10,18 +10,15 @@ char *search_cwd(char *filename, char *er)
 	DIR *dir;
 	struct dirent *sd;
 	char *ret;
-	int len = 0;
-	int i = 0;
+	int len = 0, i = 0;
 
 	while (filename[len])
 		len++;
 	ret = malloc(sizeof(char) * (len + 3));
 	dir = opendir(".");
 	if (!dir)
-	{
 		printf("Error! Unable to open directory.\n");
 		exit(0);
-	}
 	while ((sd = readdir(dir)))
 	{
 		for (i = 0; sd->d_name[i] && filename[i]; i++)
